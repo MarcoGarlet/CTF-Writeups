b1beb821b3b7fd46765ac772bc86944219a36f9d
g4rl337 <marcogarlet@gmail.com>
2022-10-23 15:26:32 +0200

    adding reply ctf

---
+++ LiveCTF/2022/reply/binary100/script/zsol.py
@@ -0,0 +1,39 @@
+from z3 import *
+
+
+
+local_888 = Real('local_888')
+local_8a0 = Real('local_8a0')
+local_918 = Real('local_918')
+local_860 = Real('local_860')
+local_919 = Real('local_919')
+local_868 = Real('local_868')
+local_920 = Real('local_920')
+local_870 = Real('local_870')
+local_921 = Real('local_921')
+local_878 = Real('local_878')
+local_8f8 = Real('local_8f8')
+local_880 = Real('local_880')
+local_8f0 = Real('local_8f0')
+local_890 = Real('local_890')
+local_8e8 = Real('local_8e8')
+local_898 = Real('local_898')
+local_8e0 = Real('local_8e0')
+local_8a8 = Real('local_8a8')
+local_8d8 = Real('local_8d8')
+local_8b0 = Real('local_8b0')
+local_8d0 = Real('local_8d0')
+local_8b8 = Real('local_8b8')
+local_8c0 = Real('local_8c0')
+local_8c8 = Real('local_8c8')
+
+solve(local_8a0==91.0,local_888 == 91.0,(local_918 + local_918 + 11.0 == local_918 + 130.0),(local_860 + local_860 + 6.0 == local_860 + 127.0),(local_919 * 7.0 == local_919 + 396.0),(local_868 == 104.0),((local_920 + 2.0) * 3.0 - 2.0 == (local_920 - 17.0) * 4.0),(local_870 == (local_870 + local_870) - 44.0),(local_921 == 67.0),((local_878 * 3.0 - 2.0) * 3.0 - (local_878 * 5.0 + 2.0) * 4.0 == local_878 * -8.0 - 146.0),((local_8f8 * 5.0 - 2.0) * 5.0 - (local_8f8 + local_8f8 + 7.0) * 6.0== local_8f8 * 33.0 - 1132.0),(local_880 == (local_921 + local_878) - 16.0),((local_8f0 + local_8f0) / 3.0 == (local_8f0 + 44.0) / 3.0),(local_890 == 49.0),(((local_8e8 * 8.0) + 15.0) * 0.1666666666666667==(local_8e8 + local_8e8 + 81.0) *0.5) ,(0.0 - local_898 / 5.0 == 36.0 - local_898),((local_8e0 * 7.0) / 2.0 == local_8e0 * 3.0 + 23.5),(local_8a8 == local_8a8 / 2.0 + 48.0),(local_8d8 == 110.0),(local_8b0 == local_8a8 / 2.0 - 1.0),(local_8d0 == 104.0),And((local_8b8 == local_8c0) , (local_8c0 == 108.0)),(local_8c8 == 48.0))
+
+
+
+
+
+
+
+
+
